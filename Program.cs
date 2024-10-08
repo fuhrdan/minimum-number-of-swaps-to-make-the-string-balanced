adc3f8e6fd1115f5afa9c51f78dad155eb5b961b
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-10-08 09:35:53 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//*****************************************************************************
+//** 1963. Minimum Number of Swaps to Make the String Balanced    leetcode   **
+//*****************************************************************************
+
+
+int minSwaps(char* s) {
+    int swaps[2] = {0};
+    int len = strlen(s);
+    int swapCount = 0;
+    for(int i = 0; i < len; i++)
+    {
+        int convert = s[i];
+        if(convert==91)
+        {
+            swaps[0]++;
+        }
+        if(convert==93)
+        {
+            swaps[1]++;
+        }
+        if(swaps[1] > swaps[0])
+        {
+            swapCount++;
+            swaps[1]--;
+            swaps[0]++;
+        }
+//        printf("s[%d] = %d\n",i,convert);
+    }
+    return swapCount;
+}
